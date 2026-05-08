f787a0c07da7afc9041602d63fb6e36d1ce019b0
leyoungje1-source <leyoungje1@gmail.com>
2026-05-08 00:09:31 +0000

    ㄱㄷㄱ

--- asdf.py
+++ asdf.py
@@ -1,12 +1,4 @@
 import streamlit as st
-
-st.title("🚀 파이썬 배우기 프로젝트")
-st.write("30317 이영제.")
-
-name = st.text_input("이름")
-if name:
-    st.success(f"{name}님, 환영합니다! 이제 코드를 수정해 문제를 해결해 보세요.")
-
 name = st.text_input("이름이 뭐야?")
 choice = st.selectbox("오늘 기분은?", ["좋음", "보통", "피곤"])
 score = st.slider("나의 에너지 점수", 0, 100)
